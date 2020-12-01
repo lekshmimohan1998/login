d6308a896d7b40de4d196207ebc581e403d18ef6
lekshmimohan <lekshmimohan1998@gmail.com>
2020-12-01 08:41:07 +0530

    File added

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My WebSite</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+ <h1>Link Ur Codes</h1>  
+ <table class="table">
+     <tr>
+         <td>Name</td>
+         <td><input class="form-control" type="text"></td>
+     </tr>
+     <tr>
+         <td>Roll Number</td>
+         <td><input class="form-control" type="text"></td>
+     </tr>
+     <tr>
+         <td>Gender</td>
+         <td>
+             <input type="radio" class="form-control" name="gender">male
+             <input type="radio" class="form-control" name="gender">female
+         </td>
+     </tr>
+     <tr>
+        <td><input class="form-control" type="checkbox"></td>
+        <td>Agree Terms & Conditions</td>
+    </tr>
+     <tr>
+         <td></td>
+         <td><button class="btn btn-warning">Register</button></td>
+     </tr>
+ </table>
+</body>
+</html>
---
+++ login_registration.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-6>col-xl-6">
+                <table class="table">
+                    <tr>
+                        <td>Email id</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-success">login</button></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                <table class="table">
+                    <tr>
+                        <td>Name</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Date of birth</td>
+                        <td><input type="date" name="" id="" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Place</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Email id</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Confirm Password</td>
+                        <td><input type="password" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-info">Register</button></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
